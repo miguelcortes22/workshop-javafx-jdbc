29afc1960155a7ee56b220d439ab796115b47b9a
Miguel Cortes <miguel_cortes22@hotmail.com>
2022-06-27 10:17:29 -0300

    Local project created

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module workshop {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires javafx.graphics;
+	
+	//exports gui;
+	
+	//opens gui to javafx.fxml;
+	//opens model.entities to javafx.base;
+	opens application to javafx.graphics, javafx.fxml;
+}
